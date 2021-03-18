254175601daa8186581f5933fba9eea9c4abc684
funcelot <artur.mustafin@gmail.com>
2021-03-18 16:20:02 +0300

    added devwem.app to yml

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,9 @@ FROM golang:alpine AS build
 RUN apk update && apk --no-cache add gcc g++ make git
 WORKDIR /go/src/app
 COPY main.go .
-RUN go mod init main
-RUN go mod tidy
-RUN GOOS=linux go build -ldflags="-s -w" -o ./bin/web-app ./main.go
+RUN go mod init main && go mod tidy && GOOS=linux go build -ldflags="-s -w" -o ./bin/web-app ./main.go
 
-FROM alpine:latest
+FROM alpine:latest as alpine
 RUN apk --no-cache add ca-certificates
 WORKDIR /usr/bin
 COPY --from=build /go/src/app/bin /go/bin
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 func main() {
-    r := mux.NewRouter()
+	r := mux.NewRouter()
 
-    r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
-    })
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>This is the homepage. Try /hello and /hello/Sammy\n</h1>")
+	})
 
-    r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
-    })
+	r.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "<h1>Hello from Docker!\n</h1>")
+	})
 
-    r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
-        vars := mux.Vars(r)
-        title := vars["name"]
+	r.HandleFunc("/hello/{name}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		title := vars["name"]
 
-        fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
-    })
+		fmt.Fprintf(w, "<h1>Hello, %s!\n</h1>", title)
+	})
 
-    http.ListenAndServe(":80", r)
+	http.ListenAndServe(":80", r)
 }
